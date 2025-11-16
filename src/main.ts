ffa8b6de4618141c196e82514df77aa7c5afa445
Fernando Arey <farey.duran@gmail.com>
2025-11-16 12:35:14 -0600

    feat: allow cors from specific domain

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const allowedOrigins = [
+  'http://localhost:5173',
+];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: false,
+  });
 
   // validations global pipe
   app.useGlobalPipes(
